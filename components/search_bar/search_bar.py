54751629766484c1e4047fd6bd1a3dc2ecf39ebd
KaiwanHassan <143492509+KaiwanHassan@users.noreply.github.com>
2024-05-29 12:24:32 +0200

    created search bar directory created search bar python file

--- components/search_bar/search_bar.py
+++ components/search_bar/search_bar.py
@@ -12,4 +12,12 @@ def search ():
     frame.grid(column=0, row=0, padx=10, pady=10, sticky=nsew)
 
 #Label
-    label= ctk.CTkLabel(frame, text=Search:)
+    label= ctk.CTkLabel(frame, text=Search:), text_color=black)
+    Label.grid(column=0, row=0, padx=10, pady=10, sticky=w)
+
+#Search Entery box
+
+    search_var =ctk.StringVar(
+    search_entry = ctk.CTkEntry(frame, width=200, textervariable= search_var)
+    search_entry.grid(column=1, row=0, padx=10, sticky= w)
+
