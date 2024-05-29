3451098ce8a0f90139c3fa2503414d8a2d120665
KaiwanHassan <143492509+KaiwanHassan@users.noreply.github.com>
2024-05-29 11:58:44 +0200

    created search bar directory created search bar python file

---
+++ components/search_bar/search_bar.py
@@ -0,0 +1,8 @@
+import customtkinter as ctk
+def search ():
+    query = search_var.get()
+    print(f'search bar: {query}')
+    root =ctk.CTk()
+    root.title(Search bar
+
+    root.columconfigure(0, weight=1)
