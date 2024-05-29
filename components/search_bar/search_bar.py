9f9a7602790ff53ce69a1db93d34517ce0f17d60
KaiwanHassan <143492509+KaiwanHassan@users.noreply.github.com>
2024-05-29 12:09:46 +0200

    created search bar directory created search bar python file

--- components/search_bar/search_bar.py
+++ components/search_bar/search_bar.py
@@ -4,5 +4,12 @@ def search ():
     print(f'search bar: {query}')
     root =ctk.CTk()
     root.title(Search bar
-
+#Grid layout
     root.columconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+# Search bar frame
+    frame =ctk.CTkLabel(frame, text=search)
+    frame.grid(column=0, row=0, padx=10, pady=10, sticky=nsew)
+
+#Label
+    label= ctk.CTkLabel(frame, text=Search:)
