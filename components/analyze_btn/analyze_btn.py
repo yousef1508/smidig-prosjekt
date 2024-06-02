da20ad34c0c8c3c1b6f56ac0dc0ddf2a636b4923
yousef1508 <yousef1508@gmail.com>
2024-06-03 01:22:48 +0200

    bug fix loading

--- components/analyze_btn/analyze_btn.py
+++ components/analyze_btn/analyze_btn.py
@@ -10,49 +10,7 @@ from volatility3.framework import contexts, constants
 from volatility3 import framework, plugins
 from tkinter import ttk
 
-# Configuration file path
-CONFIG_FILE = "config.json"
-LOG_FILE = "app.log"
 
-# Setup logging
-logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-
-def load_volatility_path():
-    if os.path.exists(CONFIG_FILE):
-        with open(CONFIG_FILE, 'r') as config_file:
-            config = json.load(config_file)
-            return config.get("VOLATILITY_PATH", None)
-    return None
-
-
-def save_volatility_path(path):
-    config = {"VOLATILITY_PATH": path}
-    with open(CONFIG_FILE, 'w') as config_file:
-        json.dump(config, config_file)
-
-
-def prompt_for_volatility_path():
-    path = filedialog.askopenfilename(
-        title="Select Volatility 3 Script",
-        filetypes=[("Python files", "*.py"), ("All files", "*.*")]
-    )
-    if path:
-        save_volatility_path(path)
-        return path
-    return None
-
-
-def get_volatility_path():
-    path = load_volatility_path()
-    if not path or not os.path.exists(path):
-        messagebox.showinfo("Volatility Path Not Found", "Please select the Volatility 3 vol.py script.")
-        path = prompt_for_volatility_path()
-        if not path:
-            messagebox.showerror("Path Selection Error", "Volatility path not set. The application will exit.")
-            exit()
-    return path
 
 
 class VolatilityApp(ctk.CTk):
